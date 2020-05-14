13551a26478acbb6a0c570240f5620be538a88b9
pbsulliv <pbsulliv@ucsc.edu>
2020-05-14 16:03:46 -0700

    Set scale on background

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -3,7 +3,20 @@ class Play extends Phaser.Scene {
       super("playScene");
     }
     
+    preload() {
+      // load background
+      this.load.image('background1', './assets/Paper.Journal.6.jpg');
+
+      // load images
+      // load asset path
+      this.load.path = './assets/Images/';
+      this.load.image([
+        { key: '8ball' },
+      ]);
+    }
+
     create() {
-      this.add.text(20, 20, "Final Game Play");
+      //place tilesprite
+      this.background1 = this.add.tileSprite(0, 0, 2379, 1791, 'background1').setOrigin(0, 0).setScale(.269,.268);
     }
   }
