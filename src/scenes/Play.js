683d4460e3db7e2d2b4f6c611fc10e980829c0ff
pbsulliv <pbsulliv@ucsc.edu>
2020-05-15 16:47:27 -0700

    Can interact with clickable objects

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -16,7 +16,50 @@ class Play extends Phaser.Scene {
     }
 
     create() {
+      // reset parameters
+      time = 0;
+
       //place tilesprite
       this.background1 = this.add.tileSprite(0, 0, 2379, 1791, 'background1').setOrigin(0, 0).setScale(.269,.268);
+
+      // create group to hold clickable objects
+      this.click = this.add.group();
+      //create the sprite
+      // create( [x] [, y] [, key] [, frame] [, visible] [, active])
+      let click = this.click.create(100,100, '8ball');
+      // make click group interactive so we can click (and remove) it
+      // https://photonstorm.github.io/phaser3-docs/Phaser.GameObjects.GameObject.html#setInteractive
+      click.setInteractive({
+        useHandCursor: true,
+      });
+      // call a function when the mouse clicks on the interactive object
+      // https://photonstorm.github.io/phaser3-docs/Phaser.Input.Events.html#event:GAMEOBJECT_POINTER_DOWN__anchor
+      click.on('pointerdown', this.removeItem);
+
+      // set up timer (triggers callback every second)
+      this.Timer = this.time.addEvent({
+        delay: 1000,
+        callback: this.timeBump,
+        callbackScope: this,
+        loop: true
+      });
+
+      //add timer to screen
+      this.timerRight = this.add.text(500, 30, time);
+
+    }
+
+    update() {
+    }
+
+    //functions
+    removeItem(pointer, localX, localY, event) {
+      let scenecxt = this.scene;  // get scene context before we kill the object
+      this.destroy();             // destroy the child obj  
+    }
+
+    timeBump() {
+      // increment level (aka score)
+      time++;
     }
   }
