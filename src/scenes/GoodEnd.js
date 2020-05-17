45929db347054bcf13f8f1844f4d13709c01e27e
pbsulliv <pbsulliv@ucsc.edu>
2020-05-16 17:29:34 -0700

    Changed GoodEnd

--- src/scenes/GoodEnd.js
+++ src/scenes/GoodEnd.js
@@ -2,4 +2,22 @@ class GoodEnd extends Phaser.Scene {
     constructor() {
       super("GoodEndScene");
     }
+
+    create() {
+        //menu configuration
+        let menuConfig = {
+          fontFamily: 'Times New Roman',
+          fontSize: '48px',
+          backgroundColor: '#000000',
+          color: '#33A2FF',
+          align: 'center',
+          padding: {
+              top:5,
+              bottom: 5,
+          },
+          fixedWidth: 0
+      }
+  
+      this.add.text(centerX, (centerY - (centerY/2))-60, ' Good End ', menuConfig).setOrigin(0.5);
+    }
 }
