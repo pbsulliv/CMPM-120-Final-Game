988dbd5ad2c16f1b78872ffc038e779d9e1baa4d
lmedikon <63619104+lmedikon@users.noreply.github.com>
2020-05-17 22:19:56 -0700

    Add files via upload

---
+++ CMPM-120-Final-Game-master/src/main.js
@@ -0,0 +1,41 @@
+'use strict';
+
+// define and configure main Phaser game object
+
+let config = {
+    parent: 'myGame',
+    type: Phaser.CANVAS,
+    height: 480,
+    width: 640,
+    scale: {
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+
+    physics: {
+        default: 'arcade',
+        arcade: {
+            //debug: true,
+            gravity: {
+                x: 0,
+                y: 0
+            }
+        }
+    },
+    scene: [ Menu, Play, LabScene2, GoodEnd, BadEnd ]
+
+}
+
+
+// define game
+let game = new Phaser.Game(config);
+
+// Define Global Vars
+let centerX = game.config.width/2;
+let centerY = game.config.height/2;
+let cursors;
+let time;
+let points;
+const inventory = {};
+
+// reserve keyboard vars
+let keySPACE;
