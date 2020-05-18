9e729ed3781b714019e8e1dd341fed176ff2fff2
pbsulliv <pbsulliv@ucsc.edu>
2020-05-17 22:41:33 -0700

    Added instructions, fixed inventory, rm extra file

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -6,6 +6,7 @@ class Menu extends Phaser.Scene {
     create() {
       time = 0;
       points = 0;
+      inventory = {};
 
 
       //menu configuration
@@ -25,8 +26,9 @@ class Menu extends Phaser.Scene {
     this.add.text(centerX, (centerY - (centerY/2))-60, ' Final Game ', menuConfig).setOrigin(0.5);
     menuConfig.fontSize = '25px';
     menuConfig.color = '#33E6FF';
-    this.add.text(centerX, centerY, ' Press Spacebar ', menuConfig).setOrigin(0.5);
-    this.add.text(centerX, centerY, ' Click the objects within 12 seconds to win! ', menuConfig).setOrigin(0.5);
+    this.add.text(centerX, centerY, ' Press Spacebar to Start', menuConfig).setOrigin(0.5);
+    this.add.text(centerX, (centerY - 80), ' Click the objects within 12 seconds to win!', menuConfig).setOrigin(0.5);
+    this.add.text(centerX, (centerY - 40), ' Be careful to find the objects you need!', menuConfig).setOrigin(0.5);
 
     // define keys
     keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
